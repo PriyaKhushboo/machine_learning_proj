68768a59c642af55a3bb0fd56f4979704b2113e0
khushboo <khushbusingh352@gmail.com>
2022-09-09 15:17:34 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from typing import List
+
+    
+# declaring variable for setup functions
+PROJECT_NAME= "housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Khushboo Singh"
+DESCRIPTION="This is the first end to end project"
+PACKAGES=["housing"]
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+
+def get_requirements_list()->List[str]: #This function will return the list which have string value in it
+    with open(REQUIREMENT_FILE_NAME) as  requireme_file:
+        requireme_file.readlines()
+
+setup(
+name = PROJECT_NAME,
+version= VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=PACKAGES,
+install_requires = get_requirements_list()
+)
